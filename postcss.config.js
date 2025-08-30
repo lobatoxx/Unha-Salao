9c01a1ffc39a07bdd9b951d633e70e7ed95db308
lobatoxx <l988389516@gmail.com>
2025-08-30 11:08:39 -0300

    Chore: Downgrade to Tailwind v3 for stability

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js (Versão Correta Final)
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
