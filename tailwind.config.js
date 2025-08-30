9eb46736ed990fee9d98193fa1eef50331b1078a
lobatoxx <l988389516@gmail.com>
2025-08-30 09:13:41 -0300

    up
    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./admin.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./admin.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pink: colors.pink,
+        purple: colors.purple,
+        blue: colors.blue,
+        green: colors.green,
+        gray: colors.gray,
+      },
+    },
+  },
   plugins: [],
 }
